3c347a27336c345a60d17dad1907fd9cb7a93909
Bob Browning <rpb6@drache.co.uk>
2012-06-07 12:32:45 +0100

    Ensure analyze mojos only executed directly

--- src/main/java/com/pressassociation/maven/wmb/mojo/MissingAnalyzeConfigurationBarMojo.java
+++ src/main/java/com/pressassociation/maven/wmb/mojo/MissingAnalyzeConfigurationBarMojo.java
@@ -3,11 +3,13 @@ package com.pressassociation.maven.wmb.mojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.jfrog.maven.annomojo.annotations.MojoGoal;
+import org.jfrog.maven.annomojo.annotations.MojoRequiresDirectInvocation;
 
 /**
 * @author Bob Browning <bob.browning@pressassociation.com>
 */
 @MojoGoal("analyze-missing")
+@MojoRequiresDirectInvocation
 public class MissingAnalyzeConfigurationBarMojo extends AbstractAnalyzeConfigurationBarMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
