56a8ef6f7f813e16f6a4291ce926c3f7c05ed116
Bob Browning <rpb6@drache.co.uk>
2013-05-13 13:01:48 +0100

    Add ability to create configurable services during deployment

---
+++ src/main/java/com/pressassociation/maven/wmb/types/ConfigurableServiceItem.java
@@ -0,0 +1,11 @@
+package com.pressassociation.maven.wmb.types;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bobb
+ * Date: 10/05/2013
+ * Time: 11:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfigurableServiceItem {
+}
